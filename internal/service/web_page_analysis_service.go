f8f303361d8ea33bed21a62e1c86513e1dbb2e6e
GayanVUPICO <gayan.bandaranayake.c@vupico.com>
2025-07-02 03:05:23 +0530

    01. Refactor the application to follow centralized error handling approach

--- internal/service/web_page_analysis_service.go
+++ internal/service/web_page_analysis_service.go
@@ -3,5 +3,5 @@ package service
 import "github.com/GayanB90/go-web-analyzer/internal/model"
 
 type WebPageAnalysisService interface {
-	AnalyzeWebPage(webPage model.WebAnalysisRequestModel) model.WebAnalysisResultModel
+	AnalyzeWebPage(webPage model.WebAnalysisRequestModel) (model.WebAnalysisResultModel, error)
 }
